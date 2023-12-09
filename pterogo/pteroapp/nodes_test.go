5f0de9a06cca5a17b419fd6a3da64ef8c11997a4
Federico Giovine <federico.giovine@gmail.com>
2023-12-09 14:22:40 +0100

    test: add GetByID test

--- pterogo/pteroapp/nodes_test.go
+++ pterogo/pteroapp/nodes_test.go
@@ -3,9 +3,35 @@ package pteroapp
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"testing"
 )
+
+func TestNodeAppGetByID(t *testing.T) {
+	env := newTestEnv()
+	defer env.Teardown()
+
+	fmt.Println(env.Application.endpoint)
+
+	env.Mux.HandleFunc("/api/application/nodes/1", func(w http.ResponseWriter, r *http.Request) {
+		testNode := Node{}
+		testNode.Attributes.ID = 1
+		json.NewEncoder(w).Encode(testNode)
+	})
+
+	ctx := context.Background()
+
+	node, _, err := env.Application.Nodes.GetByID(ctx, 1)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if node.Attributes.ID != 1 {
+		t.Errorf("unexpected user ID: %v", node.Attributes.ID)
+	}
+}
 
 func TestNodeAppCreate(t *testing.T) {
 	env := newTestEnv()
